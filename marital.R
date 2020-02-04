b5975f44578080224b2cd9bc69e8912601a8e8b2
Cai, Yun-Ting <leotsai0119@gmail.com>
2020-02-04 08:30:10 +0800

    0204

--- marital.R
+++ marital.R
@@ -3,14 +3,14 @@
 # rm
 rm(list = ls()); cat("\14")
 # source: function prop.sf.all
-source("~/Github_CFRC/PovertyRates/func_prop.sf.all.R")
+source("~/Github/PovertyRates/func_prop.sf.all.R")
 # source: functioni PovertyRates_single
-source("~/Github_CFRC/PovertyRates/func_PovertyRates_single.R")
+source("~/Github/PovertyRates/func_PovertyRates_single.R")
 # devtools::source_url("https://raw.githubusercontent.com/caiyuntingcfrc/PovertyRates/master/func_prop.sf.all.R")
 # load packages
 ins.pack("tidyverse", "feather", "parallel", "data.table", "pbapply", "haven")
 # setwd
-setwd("d:/R_wd/tw_inc/R data files/")
+setwd("i:/R_wd/tw_inc/R data files/")
 
 # cluster -----------------------------------------------------------------
 
@@ -21,41 +21,41 @@ clusterEvalQ(cl, library(data.table))
 clusterEvalQ(cl, library(epiDisplay))
 
 # load Rdata --------------------------------------------------------------
-# # read sav
-# l1 <- list.files(pattern = "^inc[7][9].*.sav|^inc[8][0-7].*.sav")
-# l2 <- list.files(pattern = "^df_inc[7][9].*.feather|^df_inc[8][0-7].*.feather")
-# path_list1 <- paste(getwd(), "/", l1, sep = "")
-# path_list2 <- paste(getwd(), "/", l2, sep = "")
-# df.list_sav <- lapply(path_list1, read_sav) %>% lapply(setDT)
-# df.list_feather <- lapply(path_list2, read_feather) %>% lapply(setDT)
-# 
-# for(i in 1:length(df.list_sav)) {
-#         
-#         df.list_sav[[i]][ , a7 := df.list_feather[[i]]$h.sex]
-#         df.list_sav[[i]][ , a8 := df.list_feather[[i]]$n.all]
-#         df.list_sav[[i]][ , a12 := df.list_feather[[i]]$n.adults]
-#         df.list_sav[[i]][ , a18 := df.list_feather[[i]]$sf]
-#         df.list_sav[[i]][ , a19 := df.list_feather[[i]]$n.elder]
-#         
-#         write_feather(df.list_sav[[i]], path_list2[[i]])
-#         
-#         }
+# read sav
+l1 <- list.files(pattern = "^inc[7][9].*.sav|^inc[8][0-7].*.sav")
+l2 <- list.files(pattern = "^df_inc[7][9].*.feather|^df_inc[8][0-7].*.feather")
+path_list1 <- paste(getwd(), "/", l1, sep = "")
+path_list2 <- paste(getwd(), "/", l2, sep = "")
+df.list_sav <- lapply(path_list1, read_sav) %>% lapply(setDT)
+df.list_feather <- lapply(path_list2, read_feather) %>% lapply(setDT)
 
-# # 90 - 107
-# l1 <- list.files(pattern = "^df_inc[1][0][0-7].*.feather|^df_inc[9][0-9].*.feather")
-# path_list1 <- paste(getwd(), "/", l1, sep = "")
-# df.list1 <- parLapply(cl, path_list1, read_feather)
-# 
-# # 79-89
-# l2 <- list.files(pattern = "^df_inc[7][9].*.feather|^df_inc[8][0-9].*.feather")
-# path_list2 <- paste(getwd(), "/", l2, sep = "")
-# df.list2 <- parLapply(cl, path_list2, read_feather)
-# 
-# 
-# # save RData --------------------------------------------------------------
-# 
-# saveRDS(df.list1, file = "df.list(90-107).rds")
-# saveRDS(df.list2, file = "df.list(79-89).rds")
+for(i in 1:length(df.list_sav)) {
+
+        df.list_sav[[i]][ , a7 := df.list_feather[[i]]$h.sex]
+        df.list_sav[[i]][ , a8 := df.list_feather[[i]]$n.all]
+        df.list_sav[[i]][ , a12 := df.list_feather[[i]]$n.adults]
+        df.list_sav[[i]][ , a18 := df.list_feather[[i]]$sf]
+        df.list_sav[[i]][ , a19 := df.list_feather[[i]]$n.elder]
+
+        write_feather(df.list_sav[[i]], path_list2[[i]])
+
+        }
+
+# 90 - 107
+l1 <- list.files(pattern = "^df_inc[1][0][0-7].*.feather|^df_inc[9][0-9].*.feather")
+path_list1 <- paste(getwd(), "/", l1, sep = "")
+df.list1 <- parLapply(cl, path_list1, read_feather)
+
+# 79-89
+l2 <- list.files(pattern = "^df_inc[7][9].*.feather|^df_inc[8][0-9].*.feather")
+path_list2 <- paste(getwd(), "/", l2, sep = "")
+df.list2 <- parLapply(cl, path_list2, read_feather)
+
+
+# save RData --------------------------------------------------------------
+
+saveRDS(df.list1, file = "df.list(90-107).rds")
+saveRDS(df.list2, file = "df.list(79-89).rds")
 
 # read rds
 df.list1 <- readRDS("df.list(90-107).rds")
--- overview.prop.R
+++ overview.prop.R
@@ -1,6 +1,6 @@
-d <- read_feather("df_inc98.feather")
+d <- read_feather("df_inc88.feather")
 setDT(d)
-d <- d[a18 %in% c(321, 322, 331, 332) , ]
+d <- d[a18 %in% c(331, 332) , ]
 l <- grep("^b16_", names(d))
 # convert b2_ to numeric
 d[ , (l) := lapply(.SD, as.character), .SDcols = l]
@@ -8,6 +8,7 @@ d[ , (l) := lapply(.SD, as.numeric), .SDcols = l]
 
 # convert b2_ to numeric
 lb2 <- grep("^b2_", names(d))
+lb16 <- grep("^b16_", names(d))
 d[ , (lb2) := lapply(.SD, as.character), .SDcols = lb2]
 d[ , (lb2) := lapply(.SD, as.numeric), .SDcols = lb2]
 m <- unique(which(d[ , ..lb2] == 1, arr.ind = TRUE))
@@ -18,4 +19,7 @@ c <- m[ , 2]
 d[r , h.select := c]
 # recode h.marital
 d[ , h.marital := .SD[[paste0("b16_", .BY$h.select)]], by = h.select]
-tab1(d$h.marital)
+epiDisplay::tab1(d$h.marital)
+
+dd <- d[ , ..lb2]
+dd2 <- d[ , ..lb16]
